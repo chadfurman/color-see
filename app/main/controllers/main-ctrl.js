c8344eb98b8cb60ef4610451782fdef4c5817c61
Chad Furman <chad@chadfurman.com>
2015-06-11 21:56:47 -0400

    initial code for extracting color from image on click

--- app/main/controllers/main-ctrl.js
+++ app/main/controllers/main-ctrl.js
@@ -1,3 +1,4 @@
+/*jslint browser:true*/
 'use strict';
 angular.module('main')
   .controller('MainCtrl', function (Camera, Config) {
@@ -9,7 +10,20 @@ angular.module('main')
     console.log('Hello from your Controller: StartCtrl in module main:. This is your controller:', this);
     // TODO: do your controller thing
 
-    angular.element(document.getElementById('new-picture')).on('click', function() {
-      console.log('click!');
+    angular.element(document.getElementById('picture')).on('click', function (event) {
+      var img = event.target;
+      var canvas = angular.element('<canvas/>')[0];
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+      var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+      var colorContainerBox = angular.element(document.getElementById('color-container'));
+      var colorBox = angular.element(document.getElementById('color'));
+      var colorHexCode = '#' + pixelData[0].toString(16) + pixelData[1].toString(16) + pixelData[2].toString(16);
+      colorBox.css('background-color', colorHexCode);
+      colorContainerBox.css('top', event.offsetY + 'px');
+      colorContainerBox.css('left', event.offsetX + 'px');
+      colorContainerBox.css('display', 'block');
+      console.log(event.offsetY, event.offsetX);
     });
   });
